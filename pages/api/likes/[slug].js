4d96fadc8c9bdd299926dd3f24f3f0c3ac7d6f85
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-02-06 10:09:38 -0500

    comment changes to slug.js

--- pages/api/likes/[slug].js
+++ pages/api/likes/[slug].js
@@ -8,21 +8,25 @@ export default async function handler (req, res) {
 
         const ipAddy = req.headers['x-forwarded-for'] ?? "0.0.0.0";
         const slug = req.query.slug;
-
+        // use IP Address from header, encrypt using a salt. Don't want to infringe on anyone's privacy or anything
         const currentUserId = createHash("md5")
         .update(ipAddy + process.env.IP_ADDRESS_SALT, "utf8")
         .digest("hex")
 
         const sessionId = slug + "___" + currentUserId;
+        // In both GET and POST method  situation, we havee to grab the likes on the post as well as the likes already
+        // added by the user
+        // Both technically return an object with a "likes" field, so account for this by retrieving the integer
+        // value stored in that field.
+        const {data: [postLikesObj] } = await supabase.from('posts').select('likes').eq('slug', slug);
+        const {data:  [userObj]  } = await supabase.from('userSessions').select('*').eq('sessionId', sessionId);
 
-        // use IP Address from header, encrypt, and salt so even I don't know what the original address is
+        const userLikes = userObj ? userObj.likes : 0;
+        const postLikes = postLikesObj.likes;
+
         switch (req.method) {
+            // Literally just return like data in the expected format
             case "GET": {
-                // For get methods, get the number of likes in the post as well as the amount of likes the user has on the post
-                const {data: [postLikesObj] } = await supabase.from('posts').select('likes').eq('slug', slug);
-                const {data:  [userLikesObj]  } = await supabase.from('userSessions').select('likes').eq('sessionId', sessionId);
-                const userLikes = userLikesObj ? userLikesObj.likes : 0;
-                const postLikes = postLikesObj.likes;
                 res.json({
                     likes: postLikes || 0,
                     currentUserLikes: userLikes || 0,
@@ -33,10 +37,8 @@ export default async function handler (req, res) {
             case "POST": {
                 // If user exists in db, increment likes in their row.
                 // If user doesn't exist, make a new row for that user and increment their likes!
-
-                const {data: [userSession]} = await supabase.from('userSessions').select('*').eq('sessionId', sessionId);
                 // Check if user session already exists
-                if (!userSession) {
+                if (!userObj) {
                     await supabase.from('userSessions').upsert({sessionId: sessionId, likes: 1});
 
                     const {data: [post]} = await supabase.from('posts').select('id').eq('slug', slug);
@@ -49,12 +51,11 @@ export default async function handler (req, res) {
                         likes: postLikeObj.likes
                     })
                 } else {
-                    let currLikes = userSession.likes;
-                    const {data: [postLikeObj]} = await supabase.from('posts').select('likes').eq('slug', slug);
+                    let currLikes = userObj.likes;
                     if (currLikes >= 3)
                         return res.json({
                             currentUserLikes: currLikes,
-                            likes: postLikeObj.likes
+                            likes: postLikesObj.likes
                         })
                     const update = await supabase.from('userSessions').update({likes: (currLikes + 1)}).eq('sessionId', sessionId);
                     const {data: [post]} = await supabase.from('posts').select('id').eq('slug', slug);
@@ -66,7 +67,7 @@ export default async function handler (req, res) {
 
                     res.json({
                         currentUserLikes: currLikes + 1,
-                        likes: postLikeObj.likes + 1
+                        likes: postLikesObj.likes + 1
                     })
                 }
                 break;
